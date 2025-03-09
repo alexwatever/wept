555566e4337cd9d48d86d9d241678e796a10cf6b
Alex Watson <alex@coded.cc>
2025-03-09 18:13:01 +1000

    Add graphql support

--- README.md
+++ README.md
@@ -4,8 +4,22 @@ Built on  [W]ordPress and L[ept]os (originally), now giving Dioxus a go.
 <br>
 
 ## Todo
+- [ ] Create schema.graphql using the wordpress example
+- [ ] Test WordPress API access
+- [ ] Add woocommerce and graphql plugin
+- [ ] Add products schema
+- [ ] Update WP settings on install:
+    - [ ] GraphQL > General Settings > Enable GraphQL Debug Mode
+    - [ ] GraphQL > General Settings > Enable Public Introspection
+    - [ ] GraphQL > CORS Settings > Add Site Address to "Access-Control-Allow-Origin" header
 
-- [ ] ...
+```shell
+http POST http://localhost:8080/graphql \
+    Content-Type:application/json \
+    <<< '{
+        "query": "{ generalSettings { url } }"
+    }'
+```
 <br>
 
 ## Usage
@@ -16,7 +30,7 @@ To serve the application, run the following command.
 
 ```shell
 npx tailwindcss -i ./input.css -o ./public/tailwind.css --watch
-dx serve
+dx serve --port 3000 --addr 0.0.0.0
 ```
 
 ### Build
@@ -31,5 +45,16 @@ To build for release, run the following command.
 
 ```shell
 dx build --release
+```
+<br>
+
+## GraphQL
+
+### Extract GraphQL Schema
+
+Use a tool like [get-graphql-schema](https://github.com/prisma-labs/get-graphql-schema) to extract the schema from a WordPress site, after enabling WPGraphQL Public Introspection.  /
+
+```shell
+get-graphql-schema http://localhost:8080/graphql > schema.graphql
 ```
 <br>
--- src/controller/products.rs
+++ src/controller/products.rs
@@ -1,5 +1,7 @@
 use dioxus::Result as DxResult;
+use graphql_client::{GraphQLQuery, Response};
 use reqwest::get;
+use tracing::info;
 
 // # Modules
 use crate::model::{
@@ -15,6 +17,14 @@ pub(crate) async fn _fetch_products(count: usize, sort: PageSort) -> DxResult<Ve
     Ok(request)
 }
 
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/graphql/schema.graphql",
+    query_path = "src/graphql/query.graphql",
+    response_derives = "Debug"
+)]
+pub struct PostsQuery;
+
 impl Products {
     /// # Fetch Products
     ///
@@ -29,14 +39,43 @@ impl Products {
     ///
     /// A list of products.
     pub(crate) async fn get(page_count: usize, page_sort: PageSort) -> DxResult<Products> {
-        let url: String =
+        let posts_endpoint = "http://localhost:8080/graphql";
+
+        let payload: posts_query::Variables = posts_query::Variables {
+            first: page_count as i64,
+            after: None,
+        };
+
+        let request_body = PostsQuery::build_query(payload);
+
+        let client = reqwest::Client::new();
+
+        let res = client
+            .post(posts_endpoint)
+            .json(&request_body)
+            .send()
+            .await?;
+
+        let response_body: Response<posts_query::ResponseData> = res.json().await?;
+
+        let posts = response_body
+            .data
+            .expect("No data received")
+            .posts
+            .expect("No posts received")
+            .nodes;
+
+        posts.into_iter().for_each(|post| {
+            info!("{:?}", post.id);
+            info!("{:?}", post.title);
+            info!("{:?}", post.content);
+            info!("{:?}", post.slug);
+        });
+
+        let dummy_endpoint: String =
             format!("https://fakestoreapi.com/products/?sort={page_sort}&limit={page_count}");
+        let products: Vec<Product> = get(&dummy_endpoint).await?.json().await?;
 
-        let request: Vec<Product> = get(&url).await?.json().await?;
-        let request: Products = Products(request);
-
-        // TODO; Get WordPress products
-
-        Ok(request)
+        Ok(Products(products))
     }
 }
